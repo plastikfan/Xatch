eb79c00ade877592da18319422fe4e04b875e69c
plastikfan <plastikfan@outlook.com>
2020-09-03 19:27:53 +0100

    (fix #9): Converter success defined by existence of output file. Fix size & handle quotes in filenames

--- Elizium.Xatch/Internal/xld-converter.ps1
+++ Elizium.Xatch/Internal/xld-converter.ps1
@@ -1,6 +1,6 @@
 
 $global:XatchXld = @{
-  Converter = [scriptblock] {
+  Converter      = [scriptblock] {
     param(
       [Parameter(Mandatory)]
       [string]$sourceFullName,
@@ -12,10 +12,25 @@ $global:XatchXld = @{
       [string]$toFormat
     )
 
-     [string]$command = ("xld -f '{0}' -o '{1}' '{2}'" `
-      -f $toFormat, $destinationAudioFilename, $sourceFullName);
+    # https://www.tecmint.com/manage-linux-filenames-with-special-characters/
+    # 02 - Olympic '93 (The Word mix).mp3
+    #
+    [string]$command = [string]::Empty;
+
+    if ($sourceFullName.Contains("'")) {
+      $command = ('xld -f "{0}" -o "{1}" "{2}"' `
+          -f $toFormat, $destinationAudioFilename, $sourceFullName)
+    }
+    else {
+      $command = ("xld -f '{0}' -o '{1}' '{2}'" `
+          -f $toFormat, $destinationAudioFilename, $sourceFullName);
+    }
 
     Invoke-Expression -Command $command;
+
+    [int]$result = (Test-Path -Path $destinationAudioFilename) ? 0 : 1;
+    Write-Debug "[*] XLD CONVERTER; running command: $command, result: $result";
+    $result;
   }
 
   DummyConverter = [scriptblock] {
